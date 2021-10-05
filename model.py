a5fe45c3ec385e131b54e9c179fe2c7845340d9b
forestwithtea <forestsoundforest@gmail.com>
2021-10-05 17:07:26 +0900

    commit

--- model.py
+++ model.py
@@ -1,12 +1,16 @@
-import os
-import numpy as np
-import tensorflow as tf
-from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Dense, Flatten, BatchNormalization
-from tensorflow.keras.preprocessing.image import load_img, img_to_array
-from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input
-from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+import cv2
 
 
-blue_path_dir = 'blue_ball/'
-red_path_dir = 'red_ball/'
+img = cv2.imread('blue_ball/blue_ball-001.jpeg')
+height, width, channels = img.shape
+
+if img is None:
+    print('이미지 불러오기 실패')
+    exit()
+
+cv2.namedWindow('Rhythm Box Slaughter')
+cv2.imshow('Rhythm Box Slaughter', img)
+
+cv2.waitKey()
+cv2.destroyAllWindows()
+
