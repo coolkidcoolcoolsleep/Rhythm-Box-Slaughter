07e25aa2de435d793c64755df11ec752b39b682c
forestwithtea <forestsoundforest@gmail.com>
2021-10-06 12:41:20 +0900

    commit

--- model.py
+++ model.py
@@ -1,16 +1,36 @@
+from tensorflow.keras.applications.resnet50 import preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array
+import tensorflow as tf
+import numpy as np
 import cv2
+import sys
 
 
-img = cv2.imread('blue_ball/blue_ball-001.jpeg')
-height, width, channels = img.shape
+vidcap = cv2.VideoCapture(0)
 
-if img is None:
-    print('이미지 불러오기 실패')
-    exit()
+if not vidcap.isOpened():
+    print('카메라를 열 수 없습니다.')
+    sys.exit()
 
-cv2.namedWindow('Rhythm Box Slaughter')
-cv2.imshow('Rhythm Box Slaughter', img)
+while vidcap.isOpened():
+    ret, frame = vidcap.read()
 
-cv2.waitKey()
+    frame = cv2.flip(frame, 1)
+    count = 0
+    if ret:
+        cv2.imshow('Rhythm Box Slaughter', frame)
+        if int(vidcap.get(1) % 100 == 0):
+            cv2.imwrite('C:/%d.png' % count, frame)
+            image = frame
+            model = tf.keras.models.load_model('model/model.h5')
+
+            count += 1
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    else:
+        break
+
+vidcap.release()
 cv2.destroyAllWindows()
-
