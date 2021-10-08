1bcc2ce2ab819a41449fea6bc6f4baaa1c668c16
forestwithtea <forestsoundforest@gmail.com>
2021-10-08 16:36:27 +0900

    one color tracking

--- ball_tracking.py
+++ ball_tracking.py
@@ -1,103 +1,74 @@
-from tensorflow.keras.applications.resnet50 import preprocess_input
-from tensorflow.keras.preprocessing.image import img_to_array
-import tensorflow as tf
-import numpy as np
 import cv2
+import time
 import sys
+import numpy as np
 
 
-class BallTracking:
-    def selective_search(self, image, method='fast'):
-        ss = cv2.ximgproc.segmentation.createSelectiveSearchSegmentation()
-        ss.setBaseImage(image)
+class Tracking:
+    def press_q(self, color):
+        img = np.full((126, 266, 3), color, np.uint8)
+        text = 'press Q key to exit'
+        cv2.putText(img, text, (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
 
-        if method == 'fast':
-            ss.switchToSelectiveSearchFast()
-        else:
-            ss.switchToSelectiveSearchQuality()
+        return img
 
-        rects = ss.process()
-        return rects
-
-    def box(self, image):
-        H, W = image.shape[:2]
-        rects = self.selective_search(image)
-
-        proposals, boxes = [], []
-
-        for (x, y, w, h) in rects:
-            if w / float(W) < 0.1 or h / float(H) < 0.1:
-                continue
-
-            roi = image[y:y + h, x:x + w]
-            roi = cv2.cvtColor(roi, cv2.COLOR_BGR2RGB)
-            roi = cv2.resize(roi, (64, 64))
-
-            roi = img_to_array(roi)
-            roi = preprocess_input(roi)
-
-            proposals.append(roi)
-            boxes.append((x, y, w, h))
-
-        proposals = np.array(proposals)
-
-        model = tf.keras.models.load_model('model/model.h5')
-        preds = model.predict(proposals)
-        preds_arg = np.argmax(preds, axis=1)
-
-        labels = {}
-
-        for i, (l, p) in enumerate(zip(preds_arg, preds)):
-            label, prob = l, np.max(p)
-
-            if prob >= 0.9:
-                (x, y, w, h) = boxes[i]
-                box = (x, y, x + w, y + h)
-
-                L = labels.get(label[i], [])
-                L.append((box, prob))
-                labels[label[i]] = L
-
-        for label in labels.keys():
-            clone = image.copy()
-
-            for (box, prob) in labels[label]:
-                (startX, startY, endX, endY) = box
-                cv2.rectangle(clone, (startX, startY), (endX, endY), (0, 255, 0), 2)
-
-            cv2.imshow('Rhythm Box Slaughter', clone)
-
-    def video_capture(self):
+    def track_and_draw(self, color_lower, color_upper, color):
         vidcap = cv2.VideoCapture(0)
+        time.sleep(0.4)
+        detections = []
 
         if not vidcap.isOpened():
             print('카메라를 열 수 없습니다.')
             sys.exit()
 
-        while vidcap.isOpened():
-            ret, frame = vidcap.read()
-
+        while True:
+            _, frame = vidcap.read()
             frame = cv2.flip(frame, 1)
+
+            if frame is None:
+                break
+
             frame = cv2.resize(frame, dsize=(266, 126))
 
-            count = 0
-            if ret:
-                if int(vidcap.get(1) % 10 == 0):
-                    cv2.imwrite('C:/%d.png' % count, frame)
-                    image = frame
-                    self.box(image)
-                    count += 1
+            blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+            hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+            mask = cv2.inRange(hsv, color_lower, color_upper)
+            mask = cv2.erode(mask, None, iterations=2)
+            mask = cv2.dilate(mask, None, iterations=2)
+            contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-                if cv2.waitKey(1) & 0xFF == ord('q'):
-                    break
+            for cnt in contours:
+                area = cv2.contourArea(cnt)
+                if area > 100:
+                    x, y, w, h = cv2.boundingRect(cnt)
+                    cv2.rectangle(frame, (x, y), (x + w, y + h), color, 3)
 
-            else:
+                    detections.append([x, y, w, h])
+
+            img = self.press_q(color)
+
+            cv2.imshow('Rhythm Box Slaughter', frame+img)
+            cv2.moveWindow(winname='Rhythm Box Slaughter', x=300, y=200)
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
 
         vidcap.release()
         cv2.destroyAllWindows()
 
+        return detections
+
 
 if __name__ == '__main__':
-    bt = BallTracking()
-    bt.video_capture()
+    red_lower = (0, 70, 50)
+    red_upper = (10, 255, 255)
+    red = (0, 0, 255)
+
+    blue_lower = (100, 150, 0)
+    blue_upper = (140, 255, 255)
+    blue = (255, 0, 0)
+
+    t = Tracking()
+
+    t.track_and_draw(red_lower, red_upper, red)
+    # t.track_and_draw(blue_lower, blue_upper, blue)
