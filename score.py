ee525df5968f8c83c7a5512533c315dd00b37acf
coolkidcoolcoolsleep <onyourmay@gmail.com>
2021-10-13 17:43:20 +0900

    ball_tracking이랑 random_rythm_box가 한 화면에 동시에 나오기는 함(random seed 수정해야 함)

--- score.py
+++ score.py
@@ -1,103 +1,191 @@
-import time
-from ball_tracking
-from random_rythm_box
+import glob, os
+from PIL import Image
+import numpy as np
+import cv2, sys
+import random
 
-
-# 점수 계산
-# 1. 랜덤좌표 끌고오고
-# 2. 인식된 자표 끌고와서
-# 3. 비교
-# 4. 점수를 부여
-# 5. 총합산
-# 6. 위너(2P)
+# 1. ball_tracking, random_rythm_box 한 화면에 띄우기
+# 2. random_box 좌표 끌고오고
+# 3. ball_tracking 좌표 끌고와서
+# 4. 2안에 3이 들어가나 비교
+# 5. 들어가면 +10, 안 들어가면 -1
+# ------------------------- 여기까지가 player1일 때
+# 5. 1p와 2p 총점수 비교
+# 6. 점수 더 높은 사람이 winner
 
 # 랜덤하게 생성된 좌표와 tracking 돼서 나온 좌표값이 일치할 때 + 10
 # 랜덤하게 생성된 좌표에 tracking 돼서 나온 좌표값이 일치하지 않을 때 -1
 
 
+class Video_Manager:
+    def __init__(self, img_width=266, img_height=126):
+        self.img_width = img_width
+        self.img_height = img_height
+        self.easy = 50
+        self.norm = 40
+        self.hard = 30
+        self.red_color = (0, 0, 255)
+        self.blue_color = (255, 0, 0)
+        self.green_color = (0, 255, 0)
+        self.white_color = (255, 255, 255)
 
-class Score:
+    def track_and_draw(self, blue_lower, blue_upper, red_lower, red_upper, level, is_one_player=True):
+        # 카메라를 VideoCapture 타입의 객체로 얻어옴
+        #  영상의 의미지를 연속적으로 캡쳐할 수 있게 하는 class
+        vidcap = cv2.VideoCapture(0)
+        detection_blue = []
+        detection_red = []
 
-    def showOnePlayerGameStats(self, elapsedTime, maxRep, currentRep):
-        widthPositionFactor = 3
-        textColor = (114, 70, 20)
-        titleScale = 1
-        titleThickness = 3
-        valueScale = 1.5
-        valueThickness = 4
+        if not vidcap.isOpened():
+            print('카메라를 열 수 없습니다.')
+            sys.exit()
 
-        progTitle = 'SCORE'
-        progTitleTopPad = 10
-        progValueTopPad = progTitleTopPad + 15
-        progValue = str(currentRep) + '/' + str(maxRep)
-        progTitleX, progTitleY, _, progTitleHeight = self.getTextPosition(progTitle, self.defaultFont, titleScale,
-                                                                          titleThickness, widthPositionFactor,
-                                                                          widthPos='right', heightPos='top')
-        progValueX, progValueY, _, _ = self.getTextPosition(progValue, self.defaultFont, valueScale, valueThickness,
-                                                            widthPositionFactor, widthPos='right', heightPos='top')
-        self.videoManager.addText(progTitle, (progTitleX, progTitleY + progTitleTopPad), self.defaultFont, titleScale,
-                                  textColor, titleThickness)
-        self.videoManager.addText(progValue, (progValueX, progValueY + progTitleHeight + progValueTopPad), self.defaultFont,
-                                  valueScale, textColor, valueThickness)
+        while True:
+            _, frame = vidcap.read()    # _: ret
+            # print(_)
+            # 영상 좌우 반전
+            frame = cv2.flip(frame, 1)
 
-        if self.player1.freezeRoundResult:
-            self.addText(timeValue, textScale=4, textThickness=8)
+            if frame is None:
+                break
 
+            frame = cv2.resize(frame, dsize=(266, 126))
 
-    def showTwoPlayerGameStats(self, elapsedTime, maxReps, currentReps):
-        widthPositionFactor = 4
-        textColor = (114, 70, 20)
-        timeTitleScale = 2
-        timeTitleThickness = 5
-        timeValueScale = 3
-        timeValueThickness = 8
-        progTitleScale = 1
-        progTitleThickness = 3
-        progValueScale = 1.5
-        progValueThickness = 4
+            # GaussianBlur: 적용해서 노이즈와 이상치 줄임
+            blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+            hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
 
-        progTitle = 'PROGRESS'
-        progTitleTopPad = 10
-        progValueTopPad = progTitleTopPad + 15
+            blue_mask = cv2.inRange(hsv, blue_lower, blue_upper)
+            # erode: 개체 경계의 픽셀을 제거
+            blue_mask = cv2.erode(blue_mask, None, iterations=2)
+            # dilate: 공백으로 구분 된 연결 영역
+            blue_mask = cv2.dilate(blue_mask, None, iterations=2)
+            # 공의 윤곽선 찾기
+            contours, _ = cv2.findContours(blue_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-        progValueP1 = str(currentReps[0]) + '/' + str(maxReps[0])
-        progTitleP1X, progTitleP1Y, _, progTitleP1Height = self.getTextPosition(progTitle, self.defaultFont, progTitleScale,
-                                                                                progTitleThickness, widthPositionFactor,
-                                                                                heightPos='top')
-        progValueP1X, progValueP1Y, _, _ = self.getTextPosition(progValueP1, self.defaultFont, progValueScale,
-                                                                progValueThickness, widthPositionFactor, heightPos='top')
-        self.videoManager.addText(progTitle, (progTitleP1X, progTitleP1Y + progTitleTopPad), self.defaultFont,
-                                  progTitleScale, textColor, progTitleThickness)
-        self.videoManager.addText(progValueP1, (progValueP1X, progValueP1Y + progTitleP1Height + progValueTopPad),
-                                  self.defaultFont, progValueScale, textColor, progValueThickness)
+            for cnt in contours:
+                area = cv2.contourArea(cnt)
+                if area > 100:
+                    x, y, w, h = cv2.boundingRect(cnt)
+                    cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 3)
+                    detection_blue.append([x, y, w, h])
+                    # print(x, y, w, h)
 
-        progValueP2 = str(currentReps[1]) + '/' + str(maxReps[1])
-        progTitleP2X, progTitleP2Y, _, progTitleP2Height = self.getTextPosition(progTitle, self.defaultFont, progTitleScale,
-                                                                                progTitleThickness, widthPositionFactor,
-                                                                                widthPos='right', heightPos='top')
-        progValueP2X, progValueP2Y, _, _ = self.getTextPosition(progValueP2, self.defaultFont, progValueScale,
-                                                                progValueThickness, widthPositionFactor, widthPos='right',
-                                                                heightPos='top')
-        self.videoManager.addText(progTitle, (progTitleP2X, progTitleP2Y + progTitleTopPad), self.defaultFont,
-                                  progTitleScale, textColor, progTitleThickness)
-        self.videoManager.addText(progValueP2, (progValueP2X, progValueP2Y + progTitleP2Height + progValueTopPad),
-                                  self.defaultFont, progValueScale, textColor, progValueThickness)
+            red_mask = cv2.inRange(hsv, red_lower, red_upper)
+            red_mask = cv2.erode(red_mask, None, iterations=2)
+            red_mask = cv2.dilate(red_mask, None, iterations=2)
+            contours, _ = cv2.findContours(red_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-        splitLine1Pt1 = (int(self.videoManager.frameWidth / 2), 0)
-        splitLine1Pt2 = (int(self.videoManager.frameWidth / 2), timeTitleY - timeTitleHeight - 5)
-        splitLine2Pt1 = (int(self.videoManager.frameWidth / 2), timeValueY + timeValueHeight + 30)
-        splitLine2Pt2 = (int(self.videoManager.frameWidth / 2), int(self.videoManager.frameHeight))
-        self.videoManager.addLine(splitLine1Pt1, splitLine1Pt2, (255, 255, 255), thickness=self.twoPlayerSplitLineThickness)
-        self.videoManager.addLine(splitLine2Pt1, splitLine2Pt2, (255, 255, 255), thickness=self.twoPlayerSplitLineThickness)
+            for cnt in contours:
+                area = cv2.contourArea(cnt)
+                if area > 100:
+                    x, y, w, h = cv2.boundingRect(cnt)
+                    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 3)
+                    detection_red.append([x, y, w, h])
+                    # print(x, y, w, h)
 
+            self.random_box(level, frame, is_one_player)
 
-        # 우승자 표시
-        winnerText = 'WINNER'
-        if self.player1.freezeRoundResult:
-            self.addText(winnerText, textScale=3, textThickness=6, widthFactor=2)
-        elif self.player2.freezeRoundResult:
-            self.addText(winnerText, textScale=3, textThickness=6, widthFactor=2, widthPos='right')
+            cv2.imshow('Rhythm Box Slaughter', frame)
 
+            # if cv2.waitKey(15) == 27:  # esc 키를 누르면 닫음
+            #     break
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        vidcap.release()
+        cv2.destroyAllWindows()
+
+        return detection_blue, detection_red
+        # 리턴값으로 좌표를 받는 건???
 
 
+    def rhythm_box_display_area(self, level, frame, is_one_player=True):
+        # game_areas = self.load_data()
+        # game_areas = frame
+        area = frame
+        display_areas = []
+        # for area in game_areas:
+        y, x, _ = area.shape  # (126, 266, 3)
+        if not is_one_player:
+            if level == 'easy':
+                area1 = (0, 0), (x//2-self.easy, y-self.easy)       # (0, 0), (103, 96)
+                area2 = (x//2, 0), (x-self.easy, y-self.easy)       # (133, 0), (236, 96)
+                # print(area1, area2)
+                display_areas.append((area1, area2))
+            if level == 'norm':
+                area1 = (0, 0), (x//2-self.norm, y-self.norm)       # (0, 0), (113, 106)
+                area2 = (x//2, 0), (x-self.norm, y-self.norm)       # (133, 0), (246, 106)
+                # print(area1, area2)
+                display_areas.append((area1, area2))
+            if level == 'hard':
+                area1 = (0, 0), (x//2-self.hard, y-self.hard)       # (0, 0), (118, 111)
+                area2 = (x//2, 0), (x-self.hard, y-self.hard)       # (133, 0), (251, 111)
+                # print(area1, area2)
+                display_areas.append((area1, area2))
+        else:
+            if level == 'easy':
+                area1 = (0, 0), (x-self.easy, y-self.easy)          # (0, 0), (236, 96)
+                # print(area1)
+                display_areas.append(area1)
+            if level == 'norm':
+                area1 = (0, 0), (x-self.norm, y-self.norm)          # (0, 0), (246, 106)
+                # print(area1)
+                display_areas.append(area1)
+            if level == 'hard':
+                area1 = (0, 0), (x-self.hard, y-self.hard)          # (0, 0), (251, 111)
+                # print(area1)
+                display_areas.append(area1)
+        return display_areas
 
+    def random_box(self, level, frame, is_one_player=True):
+        # img = self.load_data()[0]
+
+        img = frame
+        areas = self.rhythm_box_display_area(level, frame, is_one_player)
+        # (((30, 30), (103, 96)), ((163, 30), (236, 96)))
+        for area in areas:
+            if not is_one_player:
+                img = cv2.line(img, (133, 0), (133, 126), self.white_color, 2)
+                area1, area2 = area
+                (xs1, ys1), (xe1, ye1) = area1
+                (xs2, ys2), (xe2, ye2) = area2
+                a1, b1 = random.randint(xs1, xe1), random.randint(ys1, ye1)
+                a2, b2 = random.randint(xs2, xe2), random.randint(ys2, ye2)
+
+                if level == 'easy':
+                    img = cv2.rectangle(img, (a1, b1), (a1+self.easy, b1+self.easy), self.red_color, 3)
+                    img = cv2.rectangle(img, (a2, b2), (a2+self.easy, b2+self.easy), self.blue_color, 3)
+                if level == 'norm':
+                    img = cv2.rectangle(img, (a1, b1), (a1+self.norm, b1+self.norm), self.red_color, 3)
+                    img = cv2.rectangle(img, (a2, b2), (a2+self.norm, b2+self.norm), self.blue_color, 3)
+                if level == 'hard':
+                    img = cv2.rectangle(img, (a1, b1), (a1+self.hard, b1+self.hard), self.red_color, 3)
+                    img = cv2.rectangle(img, (a2, b2), (a2+self.hard, b2+self.hard), self.blue_color, 3)
+            else:
+                (xs1, ys1), (xe1, ye1) = area
+                a, b = random.randint(xs1, xe1), random.randint(ys1, ye1)
+                c, d = random.randint(xs1, xe1), random.randint(ys1, ye1)
+                if level == 'easy':
+                    img = cv2.rectangle(img, (a, b), (a + self.easy, b + self.easy), self.red_color, 3)
+                    img = cv2.rectangle(img, (c, d), (c + self.easy, d + self.easy), self.blue_color, 3)
+                if level == 'norm':
+                    img = cv2.rectangle(img, (a, b), (a + self.norm, b + self.norm), self.red_color, 3)
+                    img = cv2.rectangle(img, (c, d), (c + self.norm, d + self.norm), self.blue_color, 3)
+                if level == 'hard':
+                    img = cv2.rectangle(img, (a, b), (a + self.hard, b + self.hard), self.red_color, 3)
+                    img = cv2.rectangle(img, (c, d), (c + self.hard, d + self.hard), self.blue_color, 3)
+        # cv2.imshow('rhythm_box', img)
+        # cv2.waitKey(0)
+        # cv2.imwrite(f'data/image_output/01.jpg', img)
+
+if __name__ == '__main__':
+    blue_lower = (100, 150, 0)
+    blue_upper = (140, 255, 255)
+
+    red_lower = (0, 70, 50)
+    red_upper = (10, 255, 255)
+
+    t = Video_Manager()
+
+    t.track_and_draw(blue_lower, blue_upper, red_lower, red_upper, 'easy', is_one_player=True)
