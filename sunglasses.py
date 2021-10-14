998c47d9469851c813d4189829a6e129b549b73d
forestwithtea <forestsoundforest@gmail.com>
2021-10-14 11:16:01 +0900

    overlay

--- sunglasses.py
+++ sunglasses.py
@@ -2,37 +2,34 @@ import cv2
 import numpy as np
 
 
-# Camera
-cap_cam = cv2.VideoCapture(0)
-if not cap_cam.isOpened():
-    print('Cannot open camera')
-    exit()
-ret, frame_cam = cap_cam.read()
-if not ret:
-    print('Cannot open camera stream')
-    cap_cam.release()
-    exit()
+def sunglasses():
+    cap = cv2.VideoCapture(0)
+    if not cap.isOpened():
+        print('Cannot open camera')
+        exit()
 
-# Image
-img = cv2.imread('circular-sunglasses.png')
-img = cv2.resize(img, dsize=(266, 126))
+    width, height = 266, 126
+    img = cv2.imread('circular-sunglasses.png')
+    img = cv2.resize(img, dsize=(width, height))
 
-height = 126
-width = 266
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print('Cannot receive frame from camera')
+            break
+        frame = cv2.resize(frame, (width, height), interpolation=cv2.INTER_AREA)
 
-while True:
-    ret, frame_cam = cap_cam.read()
-    if not ret:
-        print('Cannot receive frame from camera')
-        break
-    frame_cam = cv2.resize(frame_cam, (width, height), interpolation = cv2.INTER_AREA)
+        # Blend the two images and show the result
+        tr = 0.3    # transparency between 0-1, show camera if 0
+        frame = ((1-tr) * frame.astype(np.float) + tr * img.astype(np.float)).astype(np.uint8)
 
-    # Blend the two images and show the result
-    tr = 0.3    # transparency between 0-1, show camera if 0
-    frame = ((1-tr) * frame_cam.astype(np.float) + tr * img.astype(np.float)).astype(np.uint8)
-    cv2.imshow('Transparent result', frame)
-    if cv2.waitKey(1) == 27:    # press ESC to quit
-        break
+        cv2.imshow('sunglasses', frame)
+        if cv2.waitKey(1) == 27:    # press ESC to quit
+            break
 
-cap_cam.release()
-cv2.destroyAllWindows()
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    sunglasses()
