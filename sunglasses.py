49ab2a98617bbbdb43ea61a236416bd1e99ad9b2
forestwithtea <forestsoundforest@gmail.com>
2021-10-19 14:59:46 +0900

    face and sunglasses

--- sunglasses.py
+++ sunglasses.py
@@ -1,40 +1,31 @@
 import cv2
 import cvlib as cv
+import cvzone
 
 
 def sunglasses():
+    width, height = 1330, 630
+
     cap = cv2.VideoCapture(0)
     if not cap.isOpened():
         print('Cannot open camera')
         exit()
-
-    width, height = 1330, 630
-    img = cv2.imread('sunglasses.png', -1)     # -1: 투명 영역 불러오기
-    img = cv2.resize(img, dsize=(width, height))
-
-    # RGBA이미지로부터 alpha mask 추출해서 RGB로 전환하기
-    b, g, r, a = cv2.split(img)
-    overlay_color = cv2.merge((b, g, r))
-
-    mask = cv2.medianBlur(a, 5)
 
     while True:
         ret, frame = cap.read()
         if not ret:
             print('Cannot receive frame from camera')
             break
+
         frame = cv2.resize(frame, (width, height), interpolation=cv2.INTER_AREA)
 
         # face detection
-        faces, confidences = cv.detect_face(frame)
+        faces, _ = cv.detect_face(frame)
         if faces:   # 얼굴이 인식되면 선글라스 이미지 띄우기
-            # 배경: 선글라스 뒤의 영역 black out 처리하기
-            img_bg = cv2.bitwise_and(frame.copy(), frame.copy(), mask=cv2.bitwise_not(mask))
-
-            # 전경: 선글라스 이미지에서 선글라스 mask out 처리하기
-            img_fg = cv2.bitwise_and(overlay_color, overlay_color, mask=mask)
-
-            frame = cv2.add(img_bg, img_fg)
+            for face in faces:
+                x, y, w, h = face[0], face[1], face[2], face[3]
+                glasses = cv2.imread('sunglasses.png', -1)
+                frame = cvzone.overlayPNG(frame, glasses, [x, y])
 
         cv2.imshow('sunglasses', frame)
 
