51b8922d1f3f12cb919b82955f7ae554da966891
Rawoon-Lee <rawoon@kakao.com>
2021-10-25 18:43:56 +0900

    4470개 파일만 현재 wav로 변환 완료

--- music_recommender.py
+++ music_recommender.py
@@ -2,8 +2,9 @@
 import os, glob
 import numpy as np
 import pydub
-import librosa
+import librosa.display
 import IPython.display as ipd
+import matplotlib.pyplot as plt
 
 fma_dirs = os.listdir('data/fma_small/fma_small')
 songs_dir = []
@@ -20,13 +21,35 @@ songs = [s for song in songs for s in song]
 #     if songs[i] == 'data/fma_small/fma_small/099\\099134.mp3':
 #         print(i)
 # print(songs[0][:-3])
-exit()
-for song in songs[4470:]:
-    sound = pydub.AudioSegment.from_mp3(song)
-    sound.export(f'{song[:-3]}wav', format='wav')
+# exit()
+# error: 4470
+# print(songs[4469])              # data/fma_small/fma_small/099\099096.mp3
+# print(songs[4470])              # data/fma_small/fma_small/099\099134.mp3
+# print(songs[4471])              # data/fma_small/fma_small/099\099135.mp3
+# exit()
+# sound = pydub.AudioSegment.from_mp3(songs[4470])
+# sound.export(f'{songs[4470][:-3]}wav', format='wav')
+# exit()
+# songs = songs[4470:]
+# for song in songs:
+#     sound = pydub.AudioSegment.from_mp3(song, parameters='mp3')
+#     sound.export(f'{song[:-3]}wav', format='wav')
 
-# x, sr = librosa.load('./data/fma_small/fma_small/000/000002.mp3')
-# ipd.Audio(x, rate=sr)
+songs_wav = []
+for song_dir in songs_dir:
+    songs_wav.append(glob.glob(f'data/fma_small/fma_small/{song_dir}/*.wav'))
+songs_wav = [s for song in songs_wav for s in song]
+
+# print(songs_wav)
+# print(len(songs_wav))           # 4470
+x, sr = librosa.load(songs_wav[0])
+ipd.Audio(x, rate=sr)
+
+FIG_SIZE = (15, 10)
+plt.figure(figsize=FIG_SIZE)
+librosa.display.waveplot(x, sr, alpha=0.5)
+plt.xlabel('Time')
+
 
 
 
