8fc791a13ca40f56fce36669254f6b521465aa5d
Rawoon-Lee <rawoon@kakao.com>
2021-11-02 17:57:43 +0900

    기본적인 알고리즘은 만들었으나 데이터의 숫자에서 문제가 있는 것으로 보임 확인이 필요함

--- music_recommender.py
+++ music_recommender.py
@@ -1,71 +1,27 @@
 # music_recommender.py
-import os, glob
-import numpy as np
-import pydub
-import librosa
-import librosa.display
-import IPython.display as ipd
-import matplotlib.pyplot as plt
-import itertools
-import matplotlib.pyplot as plt
-plt.style.use('seaborn-white')
+import pandas as pd
+from sklearn.metrics.pairwise import cosine_similarity
 
-fma_dirs = os.listdir('data/fma_small/fma_small')
-songs_dir = []
-for fma_dir in fma_dirs:
-    if os.path.isdir(f'data/fma_small/fma_small/{fma_dir}'):
-        songs_dir.append(fma_dir)
+data = pd.read_csv('data/collaborative/train_data.csv')
+# ValueError: Unstacked DataFrame is too big, causing int32 overflow
+# data = data.pivot_table('target', index='msno', columns='song_id')
+print(data.info())
+data = data[:10000]
+data = data.pivot_table('target', index='msno', columns='name').fillna(0)
 
-songs = []
-for song_dir in songs_dir:
-    songs.append(glob.glob(f'data/fma_small/fma_small/{song_dir}/*.mp3'))
-songs = [s for song in songs for s in song]
+print(data.head(10))
+print(data.shape)                       # (831, 258)
 
-# for i in range(8000):
-#     if songs[i] == 'data/fma_small/fma_small/099\\099134.mp3':
-#         print(i)
-# print(songs[0][:-3])
-# exit()
-# error: 4470
-# print(songs[4469])              # data/fma_small/fma_small/099\099096.mp3
-# print(songs[4470])              # data/fma_small/fma_small/099\099134.mp3
-# print(songs[4471])              # data/fma_small/fma_small/099\099135.mp3
-# exit()
-# sound = pydub.AudioSegment.from_mp3(songs[4470])
-# sound.export(f'{songs[4470][:-3]}wav', format='wav')
-# exit()
-songs = songs[4470:]
-for song in songs:
-    sound = pydub.AudioSegment.from_mp3(song, parameters='mp3')
-    sound.export(f'{song[:-3]}wav', format='wav')
-songs_wav = []
-os.makedirs('data/fma_wav')
-for song_dir in songs_dir:
-    src = glob.glob(f'data/fma_small/fma_small/{song_dir}/*.wav')
-    des = f'data/fma_wav/{src[-10:]}'
-    os.replace()
-songs_wav = [s for song in songs_wav for s in song]
+song_similarity = cosine_similarity(data, data)
+print(song_similarity.shape)                    # (831, 831)
 
-# print(songs_wav)
-# print(len(songs_wav))           # 4470
-x, sr = librosa.load(songs_wav[0])
-ipd.Audio(x, rate=sr)
+exit()
 
-fft = np.fft.fft(x)
-magnitude = np.abs(fft)
+song_similarity_df = pd.DataFrame(data=song_similarity, index=data.index, columns=data.index)
+print(song_similarity_df.head())
 
-f = np.linspace(0, sr, len(magnitude))
-print(f, f.shape)
-
-
-
-
-
-
-
-
-
-
+print(song_similarity_df.index)
+# print(song_similarity_df['SORRY| SORRY'].sort_values(ascending=False)[:5])
 
 
 
--- youtube_player.py
+++ youtube_player.py
@@ -1,32 +1,43 @@
 # youtube_player.py
 import pafy
 import vlc
+import time
 
-url = 'https://www.youtube.com/watch?v=lXDyWT3VlKg&ab_channel=M2'
-# video = pafy.new(url)
-# best = video.getbest()
-# play_url = best.url
+def player():
+    url = 'https://www.youtube.com/watch?v=lXDyWT3VlKg&ab_channel=M2'
+    # video = pafy.new(url)
+    # best = video.getbest()
+    # play_url = best.url
 
-audio = pafy.new(url)
-audio = audio.getbestaudio()
-play_url = audio.url
+    audio = pafy.new(url)
+    audio = audio.getbestaudio()
+    play_url = audio.url
 
-Instance = vlc.Instance()
-player = Instance.media_player_new()
-Media = Instance.media_new(play_url)
-Media.get_mrl()
-player.set_media(Media)
-player.play()
+    Instance = vlc.Instance()
+    player = Instance.media_player_new()
+    Media = Instance.media_new(play_url)
+    Media.get_mrl()
+    player.set_media(Media)
+    player.play()
+
+    start = time.time()
+
+    while True:
+        # if time.time() - start > 7:
+        #     player.pause()
+        #     break
+        pass
+        # stop = input('Type "s" to stop; "p" to pause; "" to play; : ')
+        # if stop == 's':
+        #     player.pause()
+        #     break
+        # elif stop == 'p':
+        #     player.pause()
+        # elif stop == '':
+        #     player.play()
 
 
-while True:
-    stop = input('Type "s" to stop; "p" to pause; "" to play; : ')
-    if stop == 's':
-        player.pause()
-        break
-    elif stop == 'p':
-        player.pause()
-    elif stop == '':
-        player.play()
+if __name__ == '__main__':
+    player()
 
 
