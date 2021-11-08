b530e26ddbd327f74004616a1b7ba0bf16d07b41
Rawoon-Lee <rawoon@kakao.com>
2021-11-08 15:42:49 +0900

    노래 목록에서 20개 곡만 겹치지 않도록 랜덤으로 골라내기(학습에 쓰인 데이터에 포함된 노래로만)

--- random_songs.py
+++ random_songs.py
@@ -2,11 +2,22 @@
 import pandas as pd
 import random
 
-df = pd.read_csv('data/collaborative/song.csv')
-num_song = len(df['name'].unique())
+df = pd.read_csv('data/collaborative/train_data_songid.csv')
+num_song_list = list(df['song_id'].unique())
+num_song = len(num_song_list)
 
 random.seed(10)
-list_20 = random.sample(range(0, num_song), 20)
-# print(list_20)
-df_20 = df.iloc[list_20]
-print(df_20[['name', 'artist_name']].values)
+list_20 = [num_song_list[n] for n in random.sample(range(0, num_song), 20)]
+print(list_20)
+print(len(list_20))
+
+df_20 = df[df['song_id'].isin(list_20)][['name', 'artist_name']].values
+# print(df_20[['name', 'artist_name']])
+print(df_20)
+print(type(df_20))
+
+result = []
+for name, singer in df_20:
+    result.append(f'{name} {singer}')
+result = set(result)
+print(len(result))
