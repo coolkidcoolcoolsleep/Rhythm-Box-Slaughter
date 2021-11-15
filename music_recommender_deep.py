c5bae38741b8128ba4b827b28632b690ec6b90b1
Rawoon-Lee <rawoon@kakao.com>
2021-11-15 18:39:53 +0900

    모든 경우의 수 데이터를 만들었고 기존 데이터에 합치면 됨

--- music_recommender_deep.py
+++ music_recommender_deep.py
@@ -1,5 +1,4 @@
 # music_recommender_deep.py
-
 import pandas as pd
 import os
 import numpy as np
@@ -9,9 +8,27 @@ from tensorflow.keras.models import Model, load_model
 from tensorflow.keras.utils import plot_model
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder
-
+from random_songs import make_csv
+from itertools import combinations
 
 song = pd.read_csv('data/collaborative/train_data.csv')
+user_max = max(song.msno.values)
+print(user_max)                     # 16508
+df_20 = make_csv()
+# df_20 = [[i+1+user_max, 1, artist, title] for i, (artist, title) in enumerate(df_20)]
+# print(df_20)
+# print(len(df_20))                   # 20
+all_combination = list(combinations(df_20, 2))
+print(all_combination)
+print(len(all_combination))
+
+all_prob = []
+for i, comb in enumerate(all_combination):
+    all_prob.append([i+1+user_max, 1, comb[0][0], comb[0][1]])
+    all_prob.append([i+1+user_max, 1, comb[1][0], comb[1][1]])
+print(all_prob)
+print(len(all_prob))
+
 print(song.info())
 
 song_listened = song.drop(['artist_name'], axis=1)
