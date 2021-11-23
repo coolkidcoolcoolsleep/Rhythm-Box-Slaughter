844181b801198db08cb94cbf4113ae3f00a901bb
Rawoon-Lee <rawoon@kakao.com>
2021-11-24 05:01:51 +0900

    크롬 + 광고차단 확장프로그램 설치: 재생은 가능하나 headless 설정불가

--- youtube_player.py
+++ youtube_player.py
@@ -2,7 +2,34 @@
 import pafy
 import vlc
 import time
-import keyboard
+from selenium import webdriver
+
+
+start = time.time()
+options = webdriver.ChromeOptions()
+# options.add_argument("headless")
+options.add_extension(r'extension_4_40_0_0.crx')
+
+driver = webdriver.Chrome('./chromedriver.exe', chrome_options=options)
+# driver = webdriver.Chrome('./extension_4_40_0_0.crx', chrome_options=options)
+
+'''유튜브 로그인'''
+
+driver.get('https://www.youtube.com/watch?v=kFLNAB5-qsQ')
+time.sleep(3)
+element = driver.find_element_by_xpath("//*[@class='ytp-large-play-button ytp-button']")
+element.click()
+
+driver.switch_to.window(driver.window_handles[-1])
+print('노래가 재생중입니다')
+time.sleep(3)
+
+
+while True:
+    time.sleep(1)
+    if time.time() - start > 40:
+        quit()
+        break
 
 
 def player():
@@ -10,17 +37,24 @@ def player():
     # video = pafy.new(url)
     # best = video.getbest()
     # play_url = best.url
-
+    test = time.time()
     audio = pafy.new(url)
+    print(time.time()-test)
     audio = audio.getbestaudio()
+    print(time.time() - test)
     play_url = audio.url
-
+    print(time.time() - test)
     Instance = vlc.Instance()
+    print(time.time() - test)
     player = Instance.media_player_new()
+    print(time.time() - test)
     Media = Instance.media_new(play_url)
+    print(time.time() - test)
     # Media = Instance.media_new(url)
     Media.get_mrl()
+    print(time.time() - test)
     player.set_media(Media)
+    print(time.time() - test)
     player.play()
 
     start = time.time()
@@ -42,9 +76,9 @@ def player():
         #     player.pause()
         # elif stop == '':
         #     player.play()
-
-
-if __name__ == '__main__':
-    player()
+#
+#
+# if __name__ == '__main__':
+#     player()
 
 
